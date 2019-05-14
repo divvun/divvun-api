ca556e03383da2bbd8058d566cf0b06e39b5375f
projektir <oprojektir@gmail.com>
2019-05-14 16:56:01 +0200

    Fix graphql issue

--- src/graphql/handlers.rs
+++ src/graphql/handlers.rs
@@ -5,11 +5,9 @@ use futures::future::Future;
 
 use crate::server::State;
 
-pub fn graphiql(_req: HttpRequest) -> HttpResponse {
-    println!("graphiql reached");
+pub fn graphiql(req: HttpRequest) -> HttpResponse {
 
-    //let html = graphiql_source(&format!("{}/graphql", &req.connection_info().host()));
-    let html = graphiql_source("127.0.0.1:8080/graphql");
+    let html = graphiql_source(&format!("http://{}/graphql", &req.connection_info().host()));
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html)
@@ -19,7 +17,7 @@ pub fn graphql(
     data: web::Data<State>,
     request: web::Json<GraphQLRequest>
 ) -> impl Future<Item=HttpResponse, Error=actix_web::Error> {
-    println!("graphql reached");
+
     web::block(move || {
         let res = request.execute(&data.graphql_schema, &());
         Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
